7eb0b86e2232365bbac36386ecbdd54d058d6740
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2026-06-09 01:26:54 +0300

    Adding error console for DB

--- backend/app.js
+++ backend/app.js
@@ -10,23 +10,25 @@ const categoryRoutes = require("./routes/categoryRoutes");
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch(err => {
+        console.error(err);
+        console.log("Failed to connect to MongoDB");
+    });
+
+app.get("/", (req, res) => {
+    res.send("Coffee Menu API Running");
+});
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/menu", menuRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/uploads",express.static(path.join(__dirname,"uploads")))
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch(err => {
-    console.error(err);
-});
-
-app.get("/", (req, res) => {
-    res.send("Coffee Menu API Running");
-});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
